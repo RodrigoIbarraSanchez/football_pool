157b6b7b7f7c83b67d7ed4f8894cf807b59f0126
Rodrigo Ibarra <roy@Rodrigos-MacBook-Pro.local>
2024-07-13 23:58:20 -0600

    routes.db devise fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
   resources :pools do
     member do
       match 'join', via: [:get, :post]
