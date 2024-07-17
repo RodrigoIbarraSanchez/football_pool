7ed38de1cbb3a98ae90826dfe4160c462f4f0069
Rodrigo Ibarra <roy@Rodrigos-MacBook-Pro.local>
2024-07-16 19:50:44 -0600

    disable or hidding jooin pool button if needed fix

--- app/javascript/components/PoolShow.jsx
+++ app/javascript/components/PoolShow.jsx
@@ -38,7 +38,7 @@ const PoolShow = ({ pool, userIsCreator, userIsParticipant, notice, currentUser,
 
       {location.pathname === `/pools/${pool.id}` && (
         <>
-          {!userIsParticipant || !pool.isFinished && (
+          {!userIsParticipant && !pool.isFinished && (
             <button className="join-button" onClick={() => window.location.href = `/pools/${pool.id}/join`} disabled={pool.isStarted}>Unirme a la Quiniela</button>
           )}
 
