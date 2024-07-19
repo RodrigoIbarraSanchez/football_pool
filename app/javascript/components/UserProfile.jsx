0564ea4ca9258f11b9243676627fab53ebef02b7
Rodrigo Ibarra <roy@Rodrigos-MacBook-Pro.local>
2024-07-19 17:06:36 -0600

    Navbar added to profile

--- app/javascript/components/UserProfile.jsx
+++ app/javascript/components/UserProfile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../stylesheets/UserProfile.css';
+import Navbar from './Navbar';
 
-const UserProfile = ({ user }) => {
+const UserProfile = ({ user, userSignedIn, csrfToken }) => {
   const navigate = useNavigate();
 
   const handleEditProfile = () => {
@@ -10,19 +11,22 @@ const UserProfile = ({ user }) => {
   };
 
   return (
-    <div className="user-profile">
-      <div className="profile-picture-container">
-        {user.profile_picture_url ? (
-          <img src={user.profile_picture_url} alt="Profile Picture" className="profile-picture" />
-        ) : (
-          <div className="profile-placeholder">{user.first_name.charAt(0).toUpperCase()}</div>
-        )}
+    <>
+      <Navbar userSignedIn={userSignedIn} csrfToken={csrfToken} />
+      <div className="user-profile">
+        <div className="profile-picture-container">
+          {user.profile_picture_url ? (
+            <img src={user.profile_picture_url} alt="Profile Picture" className="profile-picture" />
+          ) : (
+            <div className="profile-placeholder">{user.first_name.charAt(0).toUpperCase()}</div>
+          )}
+        </div>
+        <h1>{user.first_name} {user.last_name}</h1>
+        <p>Email: {user.email}</p>
+        <p>Username: {user.username}</p>
+        <button className="edit-profile-button" onClick={handleEditProfile}>Editar Perfil</button>
       </div>
-      <h1>{user.first_name} {user.last_name}</h1>
-      <p>Email: {user.email}</p>
-      <p>Username: {user.username}</p>
-      <button className="edit-profile-button" onClick={handleEditProfile}>Editar Perfil</button>
-    </div>
+    </>
   );
 };
 
