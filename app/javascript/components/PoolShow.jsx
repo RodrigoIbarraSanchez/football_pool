aa6110ba7364a91f0eb2ff1608421bb4b3f5bf88
Rodrigo Ibarra <roy@Rodrigos-MacBook-Pro.local>
2024-07-22 22:52:39 -0600

    returning dateA - dateB as match order list

--- app/javascript/components/PoolShow.jsx
+++ app/javascript/components/PoolShow.jsx
@@ -39,7 +39,7 @@ const PoolShow = ({ pool, userIsCreator, userIsParticipant, userSignedIn, notice
   const compareMatchesByDate = (a, b) => {
     const dateA = new Date(a.date);
     const dateB = new Date(b.date);
-    return dateB - dateA;
+    return dateA - dateB;
   };
 
   // Eliminar duplicados en el array de matches
