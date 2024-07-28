0057d15d5f6d58df0be643aee4ca53f2878530a1
Rodrigo Ibarra <roy@Rodrigos-MacBook-Pro.local>
2024-07-27 21:41:57 -0600

    || changed to &&

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -18,7 +18,7 @@ class Prediction < ApplicationRecord
   end
 
   def calculate_points
-    if self.match.status == 'FT' || self.match.status == 'PEN'
+    if self.match.status == 'FT' && self.match.status == 'PEN'
       if self.home_team_score == self.match.home_team_score && self.away_team_score == self.match.away_team_score
         self.points = 5
       elsif (self.home_team_score <=> self.away_team_score) == (self.match.home_team_score <=> self.match.away_team_score)
